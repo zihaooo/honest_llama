055d0bc2d4d44cce9211ca29fa06672f007f12e4
Zi-Hao WANG <zhwang0721@gmail.com>
2025-04-16 18:49:15 -0400

    feat: adapt MMLU dataset to the code

--- utils/dataset.py
+++ utils/dataset.py
@@ -125,3 +125,61 @@ def tokenized_openbookqa_gen_end_q(dataset, tokenizer):
             all_categories.append('')
 
     return all_prompts, all_labels, all_categories
+
+
+def format_mmlu(question, choice):
+    return f"Q: {question} A: {choice}"
+
+
+def format_mmlu_end_q(question, choice, rand_question):
+    return f"Q: {question} A: {choice} Q: {rand_question}"
+
+def tokenized_mmlu(dataset, tokenizer):
+    all_prompts = []
+    all_labels = []
+    for i in range(len(dataset)):
+        question = add_ending(dataset[i]['question'], '?')
+        choices = dataset[i]['choices']
+        _labels = list(range(len(choices)))
+        answer_key = dataset[i]['answer']
+
+
+        assert len(choices) == len(_labels), (len(choices), len(_labels))
+
+        for j in range(len(choices)):
+            choice = add_ending(choices[j], '.')
+            prompt = format_mmlu(question, choice)
+            if i == 0 and j == 0:
+                print(prompt)
+            prompt = tokenizer(prompt, return_tensors='pt').input_ids
+            all_prompts.append(prompt)
+
+            label = 1 if _labels[j] == answer_key else 0
+            all_labels.append(label)
+
+    return all_prompts, all_labels
+
+
+def tokenized_mmlu_gen_end_q(dataset, tokenizer):
+    all_prompts = []
+    all_labels = []
+    all_categories = []
+    for i in range(len(dataset)):
+        question = add_ending(dataset[i]['question'], '?')
+        rand_idx = np.random.randint(len(dataset))
+        rand_question =  add_ending(dataset[rand_idx]['question'], '?')
+        answer_key = dataset[i]['answer']
+        answer_by_key = dict(zip(range(len(dataset[i]['choices'])), dataset[i]['choices']))
+
+        for key, answer in answer_by_key.items():
+            answer = add_ending(answer, '.')
+            prompt = format_mmlu_end_q(question, answer, rand_question)
+            prompt = tokenizer(prompt, return_tensors='pt').input_ids
+            all_prompts.append(prompt)
+            if key == answer_key:
+                all_labels.append(1)
+            else:
+                all_labels.append(0)
+            all_categories.append('')
+
+    return all_prompts, all_labels, all_categories
